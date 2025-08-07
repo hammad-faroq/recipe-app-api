e15ee2e6e789d1b44c13e3366c58404e60c3758a
Hammad Farooq <hammadf2002@gmail.com>
2025-08-07 23:30:35 +0500

    Added the Pep8 standard for the working of linting

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,9 +1,10 @@
 from django.test import SimpleTestCase
 from app.calc import add1
 
-class  calcTests(SimpleTestCase):
+
+class calcTests(SimpleTestCase):
     """The test calss for testing the fuction defined"""
     def test_add_numbers(self):
-        res=add1(5,6)
-        self.assertEqual(res,11)
+        res = add1(5,6)
+        self.assertEqual(res, 11)
 #when we use the test prefix, the method got automaticlly picked up by the test library we are using, unittest, djnago.test, djago_rest_framwork.text etc etc
