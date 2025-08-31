8e54300a01d8c35a2d16ec9b5b3d13604b4c7544
Hammad Farooq <hammadf2002@gmail.com>
2025-08-31 18:33:27 +0500

    added the changes for the deployemtn fo the app, like nginx(reverse proxy), uwsgi and evn variables

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,12 +21,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&&@x#e$l1131a*90vg%b&jd@@lavdhyn@#x_m+@@t__62+mcha'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')#signing the django app to pretoect agaginst the scrf attack, session hijack and session cokies
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []#A whitelist of host/domain names that your Django site will serve.
+#ALLOWED_HOSTS = ['example.com', 'www.example.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
