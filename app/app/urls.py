8c74e3e7029a716267116944070719a9310c99d5
Hammad Farooq <hammadf2002@gmail.com>
2025-09-01 22:20:31 +0500

    added the new cahnges to make sure taht they are vaailable in the AWS servser sa well

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,6 +21,7 @@ from drf_spectacular.views import (
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from core import views as core_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,6 +33,7 @@ urlpatterns = [
     ),
     path("api/user/",include("user.urls")),
     path("api/recipe/", include("recipe.urls")),
+    path('api/health-check/', core_views.health_check, name='health-check'),
 ]
 
 if settings.DEBUG:#saves mdeia only in developmetn , Inproduction Nginx
